a49d0516ded6749791b1f35744945952bcdfd542
Harald Vinje <harald@xperitech.com>
2022-03-24 13:26:20 +0100

    Adding title

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -1,17 +1,23 @@
 import { ReactNode } from 'react'
-import AnimationLayout from './AnimationLayout'
-import Navbar from '../Navbar'
+import Head from 'next/head'
+import AnimationLayout from 'components/Layouts/AnimationLayout'
+import Navbar from 'components/Navbar'
 import { Toaster } from 'react-hot-toast'
 
 const BaseLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="bg-light-gray min-h-screen min-w-screen w-[100%] overflow-hidden">
-            <Navbar />
-            <AnimationLayout>
-                <main className="my-20 py-[5%] px-[10%] xl:px-[20%]">{children}</main>
-            </AnimationLayout>
-            <Toaster />
-        </div>
+        <>
+            <Head>
+                <title>Blog Site Fun 🎉</title>
+            </Head>
+            <div className="bg-light-gray min-h-screen min-w-screen w-[100%] overflow-hidden">
+                <Navbar />
+                <AnimationLayout>
+                    <main className="my-20 py-[5%] px-[10%] xl:px-[20%]">{children}</main>
+                </AnimationLayout>
+                <Toaster />
+            </div>
+        </>
     )
 }
 
