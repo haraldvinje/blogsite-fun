b963826477a91d48ec2c7d146b08e471958e72b1
Harald Vinje <haraldvinje@gmail.com>
2023-01-26 15:59:25 +0100

    Testing

--- components/PostComponents/PostItem.tsx
+++ components/PostComponents/PostItem.tsx
@@ -8,33 +8,31 @@ export const PostItem = ({ post, admin = false }: { post: Post; admin: boolean }
   const postUrl = admin ? `/admin/${post.slug}` : `/${post.username}/${post.slug}`
 
   return (
-    <Link href={postUrl}>
-      <div
-        className="my-2 w-full rounded-md border border-gray bg-white p-4
+    <div
+      className="my-2 w-full rounded-md border border-gray bg-white p-4
                 duration-300 hover:scale-105"
-      >
-        <Link href={`/${post.username}`}>
-          <strong>By @{post.username}</strong>
-        </Link>
+    >
+      <Link href={`/${post.username}`}>
+        <strong>By @{post.username}</strong>
+      </Link>
 
-        <Link href={postUrl} passHref>
-          <h1 className="my-2 text-lg font-bold hover:cursor-pointer">{post.title}</h1>
-        </Link>
+      <Link href={postUrl}>
+        <h1 className="my-2 text-lg font-bold hover:cursor-pointer">{post.title}</h1>
+      </Link>
 
-        <footer className="flex w-full justify-between">
-          <span className="mr-2 text-xs sm:text-sm">
-            {wordCount} words. {minutesToRead} min read.
+      <footer className="flex w-full justify-between">
+        <span className="mr-2 text-xs sm:text-sm">
+          {wordCount} words. {minutesToRead} min read.
+        </span>
+        {admin ? (
+          <span className="text-xs sm:text-sm">
+            {post.published ? '✅ Published' : '❌ Not published'}
           </span>
-          {admin ? (
-            <span className="text-xs sm:text-sm">
-              {post.published ? '✅ Published' : '❌ Not published'}
-            </span>
-          ) : (
-            <></>
-          )}
-          <span className="text-xs sm:text-sm">💙 {post.heartCount} hearts</span>
-        </footer>
-      </div>
-    </Link>
+        ) : (
+          <></>
+        )}
+        <span className="text-xs sm:text-sm">💙 {post.heartCount} hearts</span>
+      </footer>
+    </div>
   )
 }
